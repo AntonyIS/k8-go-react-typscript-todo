b9874262b52fd167e16750a2fa7683570fa6db8f
AntonyIS <antonyshikubu@gmail.com>
2023-07-11 11:13:18 +0300

    Adding ReactJS frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <nav className="navbar bg-body-tertiary">
+          <div className="container">
+            <span className="navbar-brand mb-0 h1">K8, Go ReactJS + TypeScript App</span>
+          </div>
+        </nav>
+        <div className='container'>
+          <div className="card mt-3 p-3">
+              <h4 className="display-6 text-capitalize">
+                Kubernetes + Go + ReactJS + TypeScript
+              </h4>
+              <div className="card-body">
+                <p className="text text-center fst-italic">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos totam eius suscipit consequatur cum autem veritatis fuga aspernatur dolorem, commodi, rem quibusdam minus consequuntur pariatur facere aut, doloribus debitis! Ipsam.
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos totam eius suscipit consequatur cum autem veritatis fuga aspernatur dolorem, commodi, rem quibusdam minus consequuntur pariatur facere aut, doloribus debitis! Ipsam.
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos totam eius suscipit consequatur cum autem veritatis fuga aspernatur dolorem, commodi, rem quibusdam minus consequuntur pariatur facere aut, doloribus debitis! Ipsam.
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos totam eius suscipit consequatur cum autem veritatis fuga aspernatur dolorem, commodi, rem quibusdam minus consequuntur pariatur facere aut, doloribus debitis! Ipsam.
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos totam eius suscipit consequatur cum autem veritatis fuga aspernatur dolorem, commodi, rem quibusdam minus consequuntur pariatur facere aut, doloribus debitis! Ipsam.
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos totam eius suscipit consequatur cum autem veritatis fuga aspernatur dolorem, commodi, rem quibusdam minus consequuntur pariatur facere aut, doloribus debitis! Ipsam.
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos totam eius suscipit consequatur cum autem veritatis fuga aspernatur dolorem, commodi, rem quibusdam minus consequuntur pariatur facere aut, doloribus debitis! Ipsam.
+                </p>
+              </div>
+          </div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
