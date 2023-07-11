49c22d49613da321da17997904dbcbc613df1f1b
AntonyIS <antonyshikubu@gmail.com>
2023-07-11 09:29:32 +0300

    Adding dynamodb implementation and wired the service to dynamodb repository

--- backend/internal/core/ports.go
+++ backend/internal/core/ports.go
@@ -1,10 +1,12 @@
 package core
 
+import "github.com/gin-gonic/gin"
+
 type TodoService interface {
 	CreateTodo(todo Todo) (*Todo, error)
 	ReadTodo(id string) (*Todo, error)
 	ReadTodos() (*[]Todo, error)
-	UpdateTodo(id string) (*Todo, error)
+	UpdateTodo(todo Todo) (*Todo, error)
 	DeleteTodo(id string) error
 }
 
@@ -12,6 +14,14 @@ type TodoRepository interface {
 	CreateTodo(todo Todo) (*Todo, error)
 	ReadTodo(id string) (*Todo, error)
 	ReadTodos() (*[]Todo, error)
-	UpdateTodo(id string) (*Todo, error)
+	UpdateTodo(todo *Todo) (*Todo, error)
 	DeleteTodo(id string) error
+}
+
+type HTTPHandler interface {
+	PostTodo(ctx *gin.Context)
+	GetTodo(ctx *gin.Context)
+	GetTodos(ctx *gin.Context)
+	UpdateTodo(ctx *gin.Context)
+	DeleteTodo(ctx *gin.Context)
 }
--- backend/internal/core/services.go
+++ backend/internal/core/services.go
@@ -1,5 +1,11 @@
 package core
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 type TodoApplicationService struct {
 	repo TodoRepository
 }
@@ -16,7 +22,7 @@ func (t *TodoApplicationService) CreateTodo(todo *Todo) (*Todo, error) {
 		This method will add an ID and created at attribute values
 		Returns the todo item after successful creation , else returns an error
 	*/
-	return nil, nil
+	return t.repo.CreateTodo(*todo)
 }
 
 func (t *TodoApplicationService) ReadTodo(id string) (*Todo, error) {
@@ -24,7 +30,7 @@ func (t *TodoApplicationService) ReadTodo(id string) (*Todo, error) {
 		Receives todo id from the API,fetches the data from the database.
 		Returns the todo item with the id else returns an error
 	*/
-	return nil, nil
+	return t.repo.ReadTodo(id)
 }
 
 func (t *TodoApplicationService) ReadTodos() (*[]Todo, error) {
@@ -32,15 +38,15 @@ func (t *TodoApplicationService) ReadTodos() (*[]Todo, error) {
 		Reads all todos in the database
 		Returns all todos from the database else returns an error
 	*/
-	return nil, nil
+	return t.repo.ReadTodos()
 }
 
-func (t *TodoApplicationService) UpdateTodo(todo *Todo) (*[]Todo, error) {
+func (t *TodoApplicationService) UpdateTodo(todo *Todo) (*Todo, error) {
 	/*
 		Updates the todo in the database if found
 		Returns an updated todo after a successful update else returns an err
 	*/
-	return nil, nil
+	return t.repo.UpdateTodo(todo)
 }
 
 func (t *TodoApplicationService) DeleteTodo(id string) error {
@@ -48,5 +54,100 @@ func (t *TodoApplicationService) DeleteTodo(id string) error {
 		Deletes a todo in the database with id
 		Returns an error if the opetation fails
 	*/
-	return nil
+	return t.repo.DeleteTodo(id)
+}
+
+type HTTPHandlerService struct {
+	svc *TodoApplicationService
+}
+
+func NewHTTPService(svc *TodoApplicationService) HTTPHandler {
+	return HTTPHandlerService{
+		svc: svc,
+	}
+}
+
+func (h HTTPHandlerService) PostTodo(ctx *gin.Context) {
+	var todo Todo
+	if err := ctx.ShouldBindJSON(&todo); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	res, err := h.svc.CreateTodo(&todo)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, res)
+	return
+}
+
+func (h HTTPHandlerService) GetTodo(ctx *gin.Context) {
+	id := ctx.Param("id")
+	user, err := h.svc.ReadTodo(id)
+
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{
+			"err": err.Error(),
+		})
+		return
+	}
+	if user == nil {
+		ctx.JSON(http.StatusNotFound, gin.H{
+			"message": "todo not found",
+		})
+		return
+	}
+	ctx.JSON(http.StatusOK, user)
+	return
+}
+func (h HTTPHandlerService) GetTodos(ctx *gin.Context) {
+	users, err := h.svc.ReadTodos()
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	ctx.JSON(http.StatusOK, users)
+	return
+}
+
+func (h HTTPHandlerService) UpdateTodo(ctx *gin.Context) {
+	var todo Todo
+	if err := ctx.ShouldBindJSON(&todo); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	res, err := h.svc.UpdateTodo(&todo)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, res)
+	return
+}
+func (h HTTPHandlerService) DeleteTodo(ctx *gin.Context) {
+	id := ctx.Param("id")
+	err := h.svc.DeleteTodo(id)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	ctx.JSON(http.StatusCreated, gin.H{
+		"message": "todo deleted successfully",
+	})
+	return
 }
