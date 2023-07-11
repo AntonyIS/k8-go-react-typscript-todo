3625f2f9a33f5509bb2267f8b00109a4ae6c3e11
AntonyIS <antonyshikubu@gmail.com>
2023-07-11 09:41:25 +0300

    Adding HTTP routes

--- backend/internal/adapters/http/app.go
+++ backend/internal/adapters/http/app.go
@@ -1,7 +1,43 @@
 package http
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
 
+	"github.com/AntonyIS/k8-go-react-typscript-todo/backend/config"
+	"github.com/AntonyIS/k8-go-react-typscript-todo/backend/core"
+	"github.com/AntonyIS/k8-go-react-typscript-todo/backend/internal/core"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
 
+func InitHTTPRoutes(svc *core.HTTPHandlerService, c config.Config) {
+	// Enable detailed error responses
+	gin.SetMode(gin.DebugMode)
 
-func 
+	// Setup Gin router
+	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
+
+	// Group users API
+	todoroutes := router.Group("/api/v1/todo")
+
+	handler := core.NewHTTPService(svc)
+
+	{
+		todoroutes.GET("/", handler.GetUsers)
+		todoroutes.GET("/:id", handler.GetUser)
+		todoroutes.POST("/", handler.PostUser)
+		todoroutes.PUT("/:id", handler.PutUser)
+		todoroutes.DELETE("/:id", handler.DeleteUser)
+	}
+	port := fmt.Sprintf(":%s", c.Port)
+
+	router.Run(port)
+}
