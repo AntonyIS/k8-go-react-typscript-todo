943b9bd92fbed6189f385379029c03537b04e8b5
AntonyIS <antonyshikubu@gmail.com>
2023-07-13 08:47:54 +0300

    Adding Docker images for both front end backend applications

--- backend/config/config.go
+++ backend/config/config.go
@@ -35,7 +35,7 @@ func NewConfiguration() Config {
 }
 
 func LoadEnv() error {
-	err := godotenv.Load("../.env")
+	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatal("Error loading .env file", err)
 	}
